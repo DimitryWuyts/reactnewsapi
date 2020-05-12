8cecdb20ead82df04f5f2e9c3b7b25c203f77d03
Dimitry Wuyts <dimiw1337@hotmail.com>
2020-05-12 12:30:42 +0200

    getting first article returned

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App  (){
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  async function fetchData() {
+    const response = await fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=9M5hu4ALkj0IjvHANhZ4YsC8NLAzW5dn');
+    const json = await response.json();
+    setData(json.response.docs);
+    setLoading(false)
+    console.log(json.response.docs)
+  }
+
+  useEffect(() => {
+    fetchData()
+
+  }, []);
+
+  return (<div>
+        {data.map(article => (
+            <h1 key ={article.id}>{article.abstract}</h1>
+        ))}
+  </div>);
+};
 
 export default App;
